14409a156f425587090ca57b037cd65b657d696e
Wilber Godoy <37167412+Wilberhg@users.noreply.github.com>
2024-06-19 02:30:51 -0300

    Update mergePDF2.py

--- mergePDF2.py
+++ mergePDF2.py
@@ -1,25 +1,29 @@
-from PyPDF2 import PdfFileMerger
+from PyPDF2 import PdfMerger
 from glob import glob
-from os import getcwd
+from os import getcwd, path, makedirs
+from shutil import move
 from traceback import format_exc
 from time import sleep
 
 try:
-    print('\nInicializando o programa...')
+    print("\nInicializando o programa...")
     varDir = getcwd()
-    pdfs = glob(rf'{varDir}\*.pdf')
-    print('\nColetando o nome das pastas e dos arquivos...')
-    merger = PdfFileMerger(strict=False)
-    for pdf in pdfs:
-        merger.append(pdf)
-    print('\nJuntando os arquivos PDFs...')
-    merger.write(rf'{varDir}\RENOMEAR.pdf')
-    merger.close()
-    print('\nPrograma concluído com êxito!')
+    pdfs = glob(path.normpath(path.join(varDir, "*.pdf")))
+    if pdfs:
+        print("\nColetando o nome das pastas e dos arquivos...")
+        with PdfMerger(strict=False) as merger:
+            for pdf in pdfs:
+                merger.append(pdf)
+            print("\nJuntando os arquivos PDFs...")
+            merger.write(path.normpath(path.join(varDir, "RENOMEAR.pdf")))
+        diretorio_backup = path.normpath(path.join(varDir, "arquivos_iniciais"))
+        makedirs(diretorio_backup)
+        for pdf in pdfs:
+            move(pdf, diretorio_backup)
+    print("\nPrograma concluído com êxito!")
     sleep(2.5)
 except:
-    print('\nO programa apresentou erro e está sendo registrado...')
+    print("\nO programa apresentou erro e está sendo registrado...")
     sleep(5)
-    file = open(rf'{varDir}\ErrorLog.txt', 'a+')
-    file.write(format_exc)
-    file.close()
+    with open(path.normpath(path.join(varDir, "ErrorLog.txt")), "a+") as file:
+        file.write(format_exc())
